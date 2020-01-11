4aaec0eadb9b28bd75ef6a549b74ea7b7f12bc48
Marius Gebhardt <marius.gebhardt@gmail.com>
2020-01-11 17:29:58 +0100

    bump iOS SDK version

--- ios/CleverPush.h
+++ ios/CleverPush.h
@@ -6,6 +6,17 @@
 #import <UserNotifications/UserNotifications.h>
 #endif
 
+#import "CPChatView.h"
+
+@interface CPNotificationReceivedResult : NSObject
+
+@property(readonly)NSDictionary* payload;
+@property(readonly)NSDictionary* notification;
+@property(readonly)NSDictionary* subscription;
+-(instancetype)initWithPayload:(NSDictionary *)payload;
+
+@end;
+
 @interface CPNotificationOpenedResult : NSObject
 
 @property(readonly)NSDictionary* payload;
@@ -20,6 +31,7 @@ typedef void (^CPFailureBlock)(NSError* error);
 
 typedef void (^CPHandleSubscribedBlock)(NSString * result);
 
+typedef void (^CPHandleNotificationReceivedBlock)(CPNotificationReceivedResult * result);
 typedef void (^CPHandleNotificationOpenedBlock)(CPNotificationOpenedResult * result);
 
 extern NSString * const kCPSettingsKeyInFocusDisplayOption;
@@ -30,26 +42,33 @@ extern NSString * const CLEVERPUSH_SDK_VERSION;
 
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback;
++ (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationReceived:(CPHandleNotificationReceivedBlock)receivedCallback handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback autoRegister:(BOOL)autoRegister;
++ (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationReceived:(CPHandleNotificationReceivedBlock)receivedCallback handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback autoRegister:(BOOL)autoRegister;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback autoRegister:(BOOL)autoRegister;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback;
++ (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationReceived:(CPHandleNotificationReceivedBlock)receivedCallback handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback;
++ (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId
+   handleNotificationReceived:(CPHandleNotificationReceivedBlock)receivedCallback
+   handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback autoRegister:(BOOL)autoRegister;
 + (id)initWithLaunchOptions:(NSDictionary*)launchOptions channelId:(NSString*)channelId handleNotificationOpened:(CPHandleNotificationOpenedBlock)openedCallback handleSubscribed:(CPHandleSubscribedBlock)subscribedCallback autoRegister:(BOOL)autoRegister;
 
 + (NSString*)channelId;
 
 + (BOOL)isSubscribed;
 + (void)subscribe;
++ (void)subscribe:(CPHandleSubscribedBlock)subscribedBlock;
 + (void)unsubscribe;
 
 + (void)didRegisterForRemoteNotifications:(UIApplication*)app deviceToken:(NSData*)inDeviceToken;
 + (void)handleDidFailRegisterForRemoteNotification:(NSError*)err;
++ (void)handleNotificationOpened:(NSDictionary*)messageDict isActive:(BOOL)isActive;
 + (void)handleNotificationReceived:(NSDictionary*)messageDict isActive:(BOOL)isActive;
-+ (void)handleNotificationReceived:(NSDictionary*)messageDict isActive:(BOOL)isActive wasOpened:(BOOL)wasOpened;
 + (BOOL)handleSilentNotificationReceived:(UIApplication*)application UserInfo:(NSDictionary*)messageDict completionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
 + (UNMutableNotificationContent*)didReceiveNotificationExtensionRequest:(UNNotificationRequest*)request withMutableNotificationContent:(UNMutableNotificationContent*)replacementContent;
 + (UNMutableNotificationContent*)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest*)request withMutableNotificationContent:(UNMutableNotificationContent*)replacementContent;
@@ -62,6 +81,7 @@ extern NSString * const CLEVERPUSH_SDK_VERSION;
 + (void)removeSubscriptionTag:(NSString*)tagId;
 + (void)setSubscriptionAttribute:(NSString*)attributeId value:(NSString*)value;
 + (NSArray*)getAvailableTags;
++ (NSArray*)getAvailableTopics;
 + (NSDictionary*)getAvailableAttributes;
 + (NSArray*)getSubscriptionTags;
 + (BOOL)hasSubscriptionTag:(NSString*)tagId;
@@ -72,9 +92,17 @@ extern NSString * const CLEVERPUSH_SDK_VERSION;
 + (NSMutableArray*)getSubscriptionTopics;
 + (void)setSubscriptionTopics:(NSMutableArray *)topics;
 + (void)setBrandingColor:(UIColor *)color;
++ (UIColor*)getBrandingColor;
++ (void)setChatBackgroundColor:(UIColor *)color;
++ (UIColor*)getChatBackgroundColor;
++ (void)setAutoClearBadge:(BOOL)autoClear;
++ (void)addChatView:(CPChatView*)chatView;
 + (void)showTopicsDialog;
 + (void)showAppBanners;
 + (void)showAppBanners:(void(^)(NSString *))urlOpenedCallback;
++ (void)reLayoutAppBanner;
 + (NSArray*)getNotifications;
++ (NSDictionary*)getChannelConfig;
++ (NSString*)getSubscriptionId;
 
 @end
