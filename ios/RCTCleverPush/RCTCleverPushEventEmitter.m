0f8a905df73791978cb3093cf12ded470326de46
cpandya25 <c.pandya.ext@cleverpush.com>
2026-08-21 16:11:57 +0530

    feature/cp-12390-react-native-sdk-implement-setnotificationdelivered
    implemented trackNotificationDelivered and trackNotificationClicked  public methods for manually reporting notification delivered and clicked events.

--- ios/RCTCleverPush/RCTCleverPushEventEmitter.m
+++ ios/RCTCleverPush/RCTCleverPushEventEmitter.m
@@ -193,12 +193,12 @@ RCT_EXPORT_METHOD(setSubscriptionCountry:(NSString *)country) {
     [CleverPush setSubscriptionCountry:country];
 }
 
-RCT_EXPORT_METHOD(setNotificationDelivered:(NSString *)notificationId) {
-    [CleverPush setNotificationDelivered:notificationId];
+RCT_EXPORT_METHOD(trackNotificationDelivered:(NSString *)notificationId) {
+    [CleverPush trackNotificationDelivered:notificationId];
 }   
 
-RCT_EXPORT_METHOD(setNotificationClicked:(NSString *)notificationId) {
-    [CleverPush setNotificationClicked:notificationId];
+RCT_EXPORT_METHOD(trackNotificationClicked:(NSString *)notificationId) {
+    [CleverPush trackNotificationClicked:notificationId];
 }   
 
 RCT_EXPORT_METHOD(subscribe) {
